7e270e66e3f5300572cb859406bde43d3410fcf9
Adrià Mestres <amestreszk@gmail.com>
2022-02-03 11:44:02 +0100

    Modal mostrando el nombre de esa moneda

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ import '/css/styles.css'
 
 const input = document.querySelector("#nombre-moneda");
 const boton = document.querySelector("#buscar");
+const modal = document.querySelector(".myModal");
+const ArrayMonedas = [];
+let contador = 0;
 
 
 boton.addEventListener("click", async () => {
@@ -12,11 +15,18 @@ boton.addEventListener("click", async () => {
   for (let i = 0; i < response.data.length; i++) {
     let nombre = response.data[i].name;
     let moneda = input.value;
-    if(nombre == moneda){
-      AddList(response.data[i]);
-      i = response.data.length;
+    if(nombre == moneda){ 
+      if(!ArrayMonedas.includes(nombre)){
+        ArrayMonedas[contador] = nombre;
+        contador++;
+        AddList(response.data[i]);
+        i = response.data.length;
+        input.value = "";
+      }
+      else{
+        alert("Ya has añadido la moneda a la lista");
+      }
       encontrada = true;
-      input.value = "";
     }
   }
   if(encontrada === false){
@@ -27,17 +37,33 @@ boton.addEventListener("click", async () => {
 const AddList = async (moneda) => {
   let lista = document.querySelector(".container-lista");
   let div = document.createElement("div");
-  div.className = `divNuevo moneda${moneda.name}`
+  div.className = `divNuevo moneda${moneda.name}`;
+  div.addEventListener("click", () => AbrirInfo(moneda.name))  //cuando hacemos click encima del div de una moneda, se abre  un modal con información de la misma
   lista.appendChild(div);
+
   let pSymbol = document.createElement("p");
   pSymbol.textContent = moneda.symbol;
   pSymbol.className = "pNuevo"
+
   let pName = document.createElement("p");
   pName.textContent = moneda.name;
   pName.className = "pNuevo"
+
   div.appendChild(pSymbol);
   div.appendChild(pName);
 };
+
+function AbrirInfo(nombre){
+  modal.style.display = "block"; //cambiamos el atributo "display" a block y así hacemos que sea vea en pantalla
+  let texto = document.querySelector(".texto-modal");
+  texto.textContent = nombre;
+}
+
+window.onclick = function(event) {  //captura el click afuera del modal y así poder cerrarlo
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 const AddTrending = async () => {
   let lista = document.querySelector(".container-lista-trending");
@@ -57,7 +83,6 @@ const AddTrending = async () => {
     div.appendChild(pName);
   }
 };
-
 
 AddTrending();
 
