1cb48e04dd79f34b76fd559d0f74d4e4dd338c93
Adrià Mestres <amestreszk@gmail.com>
2022-02-04 12:21:44 +0100

    abrir modal con info de la moneda

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ const AddList = async (moneda) => {
   let lista = document.querySelector(".container-lista");
   let div = document.createElement("div");
   div.className = `divNuevo moneda${moneda.name}`;
-  div.addEventListener("click", () => AbrirInfo(moneda.name))  //cuando hacemos click encima del div de una moneda, se abre  un modal con información de la misma
+  div.addEventListener("click", () => AbrirInfo(moneda.id))  //cuando hacemos click encima del div de una moneda, se abre  un modal con información de la misma
   lista.appendChild(div);
 
   let pSymbol = document.createElement("p");
@@ -53,10 +53,24 @@ const AddList = async (moneda) => {
   div.appendChild(pName);
 };
 
-function AbrirInfo(nombre){
+const AbrirInfo = async(id) =>{
+  const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${id}`);
+
+  let imagen = document.querySelector(".imagen-moneda");
+  imagen.src = response.data.image.large;
+
+  let simbolo = document.querySelector(".simbolo-moneda");
+  simbolo.textContent = `Símbolo: ${response.data.symbol}`;
+
+  let nombre = document.querySelector(".nombre-moneda");
+  nombre.textContent = `Nombre: ${response.data.name}`;
+
+  if(response.data.links.homepage[0] != null){
+    let link = document.querySelector(".link-moneda");
+    link.textContent = response.data.links.homepage[0];
+    link.href = response.data.links.homepage[0];
+  }
   modal.style.display = "block"; //cambiamos el atributo "display" a block y así hacemos que sea vea en pantalla
-  let texto = document.querySelector(".texto-modal");
-  texto.textContent = nombre;
 }
 
 window.onclick = function(event) {  //captura el click afuera del modal y así poder cerrarlo
@@ -70,15 +84,19 @@ const AddTrending = async () => {
   const response = await axios.get(`https://api.coingecko.com/api/v3/search/trending`);
   for (let i = 0; i < response.data.coins.length; i++) {
     let moneda = response.data.coins[i];
+
     let div = document.createElement("div");
     div.className = "divNuevo"
     lista.appendChild(div);
+
     let pSymbol = document.createElement("p");
     pSymbol.textContent = moneda.item.symbol;
-    pSymbol.className = "pTrending"
+    pSymbol.className = "pTrending-Simbolo"
+
     let pName = document.createElement("p");
     pName.textContent = moneda.item.name;
-    pName.className = "pTrending"
+    pName.className = "pTrending-Nombre"
+    
     div.appendChild(pSymbol);
     div.appendChild(pName);
   }
